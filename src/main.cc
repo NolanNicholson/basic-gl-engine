4dfd401f47e3e1e01d9015814219f233bbf34c00
Nolan Nicholson <nolananicholson@gmail.com>
2020-03-12 20:10:37 -0700

    now using App object

--- src/main.cc
+++ src/main.cc
@@ -15,56 +15,79 @@ void framebuffer_size_callback(GLFWwindow* window,
   glViewport(0, 0, width, height);
 }
 
+class App {
+  public:
+    GLFWwindow *window;
+    GLuint program;
+    GLuint VAO;
+
+    bool initialize() {
+      std::cout << "Starting..." << std::endl;
+
+      //Initialize GLFW
+      if (!glfwInit()) {
+        return false;
+      }
+
+      //Create a GLFW window running OpenGL 4.5
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+      window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+      if (!window) {
+        glfwTerminate();
+        return false;
+      }
+
+      //Set GLFW window context and other properties
+      glfwMakeContextCurrent(window);
+      glfwSwapInterval(1); // wait for vsync
+      glfwSetKeyCallback(window, key_callback);
+      glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+      //Initialize OpenGL and load shaders
+      if (!init_opengl()) {
+        std::cerr << "Failed to initialize OpenGL!" << std::endl;
+        return false;
+      }
+
+      if (!load_shaders(program)) {
+        std::cerr << "Failed to load/compile shaders - terminating" << std::endl;
+        return false;
+      }
+
+      //Set up VAO and VBO
+      glCreateVertexArrays(1, &VAO);
+      glBindVertexArray(VAO);
+
+      //Done!
+      return true;
+    }
+
+    void shutdown() {
+      glDeleteVertexArrays(1, &VAO);
+      glDeleteProgram(program);
+      glfwTerminate();
+    }
+};
+
+
 int main(int argc, char* args[]) {
-  std::cout << "Starting..." << std::endl;
-
-  //Initialize GLFW
-  if (!glfwInit()) {
-    return -1;
-  }
-
-  //Create a GLFW window running OpenGL 4.5
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-  GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  if (!window) {
-    glfwTerminate();
-    return -1;
-  }
-
-  //Set GLFW window context and other properties
-  glfwMakeContextCurrent(window);
-  glfwSwapInterval(1); // wait for vsync
-  glfwSetKeyCallback(window, key_callback);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  //Initialize OpenGL and load shaders
-  if (!init_opengl()) {
-    std::cerr << "Failed to initialize OpenGL - terminating" << std::endl;
+  //Create and initialize an application instance
+  App a;
+  if (!a.initialize()) {
+    std::cerr << "Failed to start application - terminating" << std::endl;
     return 1;
   }
-  GLuint program;
-  if (!load_shaders(program)) {
-    std::cerr << "Failed to load/compile shaders - terminating" << std::endl;
-    return 1;
-  }
-
-  //Set up a VAO (empty, but needs to be initialized for GL to draw anything?)
-  GLuint VAO;
-  glCreateVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
 
   //Main loop
   std::cout << "Rendering..." << std::endl;
-  while (!glfwWindowShouldClose(window)) {
-    render(program);
-    glfwSwapBuffers(window);
+  while (!glfwWindowShouldClose(a.window)) {
+    render(a.program);
+    glfwSwapBuffers(a.window);
     glfwPollEvents();
   }
 
   //Wrap up
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteProgram(program);
-  glfwTerminate();
+  a.shutdown();
   return 0;
 }
