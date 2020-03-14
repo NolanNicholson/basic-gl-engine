d1c645cc826c21738481f874ee591565e6415f89
Nolan Nicholson <nolananicholson@gmail.com>
2020-03-14 14:27:17 -0700

    use dynamic arrays for Object positions+colors

--- src/object.h
+++ src/object.h
@@ -2,12 +2,14 @@ class Object {
   public:
     GLuint VAO;
     GLuint VBO[2];
-    int vertex_count;
+    GLfloat *positions, *colors;
+    int vertex_count = 0;
 
     void initialize() {
       //Position and color data
       vertex_count = 6;
-      static const GLfloat positions[] = {
+      //GLfloat positions[] = {
+      positions = new GLfloat[vertex_count * 4] {
          0.0,  0.0,  0.5,  1.0,
          0.4,  0.4,  0.5,  1.0,
          0.0,  0.4,  0.5,  1.0,
@@ -17,7 +19,7 @@ class Object {
       };
       static const GLfloat yellow[] = { 1.0, 0.7, 0.3, 1.0 };
       static const GLfloat red[]    = { 1.0, 0.3, 0.3, 1.0 };
-      static const GLfloat colors[] = {
+      colors = new GLfloat[vertex_count * 4] {
         yellow[0], yellow[1], yellow[2], yellow[3],
         yellow[0], yellow[1], yellow[2], yellow[3],
         yellow[0], yellow[1], yellow[2], yellow[3],
@@ -26,6 +28,10 @@ class Object {
         red[0], red[1], red[2], red[3],
       };
 
+      init_buffers();
+    }
+
+    void init_buffers() {
       //Set up VAO
       glGenVertexArrays(1, &VAO);
       glBindVertexArray(VAO);
@@ -34,7 +40,8 @@ class Object {
       glCreateBuffers(2, &VBO[0]);
 
       //VBO 0: Positions
-      glNamedBufferStorage(VBO[0], sizeof(positions), positions, 0);
+      int positions_size = vertex_count * 4 * sizeof(GLfloat);
+      glNamedBufferStorage(VBO[0], positions_size, positions, 0);
       //Set up the VAO and VBO to feed the vertex shader
       glVertexArrayVertexBuffer(VAO,
           0, //first vertex buffer binding
@@ -43,7 +50,7 @@ class Object {
           4 * sizeof(GLfloat) // each vertex is one vec4
           );
       glVertexArrayAttribFormat(VAO,
-          0,
+          0, //first vertex buffer binding
           4, // number of components
           GL_FLOAT,
           GL_FALSE, // don't normalize
@@ -52,7 +59,8 @@ class Object {
       glEnableVertexArrayAttrib(VAO, 0);
 
       //VBO 1: Colors
-      glNamedBufferStorage(VBO[1], sizeof(colors), colors, 0);
+      int colors_size = vertex_count * 4 * sizeof(GLfloat);
+      glNamedBufferStorage(VBO[1], colors_size, colors, 0);
       glVertexArrayVertexBuffer(VAO, 1, VBO[1], 0, 4 * sizeof(GLfloat));
       glVertexArrayAttribFormat(VAO, 1, 4, GL_FLOAT, GL_FALSE, 0);
       glEnableVertexArrayAttrib(VAO, 1);
@@ -67,5 +75,10 @@ class Object {
       glDeleteVertexArrays(1, &VAO);
       glDeleteBuffers(1, &VBO[0]);
       glDeleteBuffers(1, &VBO[1]);
+    }
+
+    ~Object() {
+      delete [] positions;
+      delete [] colors;
     }
 };
